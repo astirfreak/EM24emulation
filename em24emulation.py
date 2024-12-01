54d66fa68d8dba01ea58f01e8574624ad101162f
astirfreak <122223155+astirfreak@users.noreply.github.com>
2024-12-01 21:38:40 +0100

    Update em24emulation.py

--- em24emulation.py
+++ em24emulation.py
@@ -16,13 +16,12 @@
 
 from pyModbusTCP.server import ModbusServer, DataBank
 import time
-import socket
-import select
 
 # values you like to publish, need to get filled somehow
 phase_voltages = [230.0, 230.0, 230.0]  # Volt 
 phase_currents = [0.0, 0.0, 0.0]        # Ampere 
 phase_powers = [0.0, 0.0, 0.0]          # Watt
+total_power = [0.0]                     # Watt
 
 # converts python-numbers into two 16 bit words for the Modbus-registers
 def Words(value):
@@ -64,8 +63,9 @@ try:
         server.data_bank.set_holding_registers(0x10, Words(phase_currents[2]*1000)) # current L3
         server.data_bank.set_holding_registers(0x12, Words(phase_powers[0]*10))     # power L1
         server.data_bank.set_holding_registers(0x14, Words(phase_powers[1]*10))     # power L2
-        server.data_bank.set_holding_registers(0x16, Words(phase_powers[2]*10))     # power L3        
-        # total_power is not required by Venus, it sums up the 3 phase values.
+        server.data_bank.set_holding_registers(0x16, Words(phase_powers[2]*10))     # power L3
+        server.data_bank.set_holding_registers(0x28, Words(total_power*10))         # totalPower
+        
         time.sleep(0.1)
         pass  # server is running until stopped manualy
 
